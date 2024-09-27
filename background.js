f9963776c932637998dccecf195715d5ba53a8d4
nickfu <net456@163.com>
2024-09-28 02:42:46 +0800

    添加屏蔽webrtc功能

--- background.js
+++ background.js
@@ -1,82 +1,48 @@
-// chrome.storage.local.get(['tiny_proxy_scheme', 'tiny_proxy_host', 'tiny_proxy_port', 'tiny_proxy_desc'], function(res) {
-//   let scheme = res.tiny_proxy_scheme;
-//   let host = res.tiny_proxy_host;
-//   let port = parseInt(res.tiny_proxy_port);
-//   console.log('aaaa', scheme, host, port)
-//   let proxy_config = {
-//     scheme: scheme,
-//     host: host,
-//     port: port
-//   }
-//   console.log('bbbb', proxy_config)
-//   let config = {
-//     mode: "fixed_servers",
-//     rules: {
-//       proxyForHttp: proxy_config,
-//       proxyForHttps: proxy_config,
-//       bypassList: ["foobar.com"]
-//     }
-//   };
-//   chrome.proxy.settings.set(
-//     {value: config, scope: 'regular'},
-//     function() {
-//       console.log('ccccc')
-//     }
-//   );
-// });
+const updateFunction = async function () {
+  const res = await chrome.storage.local.get([
+    "tiny_proxy_scheme",
+    "tiny_proxy_host",
+    "tiny_proxy_port",
+    "tiny_proxy_desc",
+    "tiny_proxy_block_webrtc",
+  ]);
+
+  let scheme = res.tiny_proxy_scheme;
+  let host = res.tiny_proxy_host;
+  let port = parseInt(res.tiny_proxy_port);
+  console.log("aaaa", scheme, host, port);
+  let proxy_config = {
+    scheme: scheme,
+    host: host,
+    port: port,
+  };
+  console.log("bbbb", proxy_config);
+  let config = {
+    mode: "fixed_servers",
+    rules: {
+      proxyForHttp: proxy_config,
+      proxyForHttps: proxy_config,
+      proxyForFtp: proxy_config,
+      bypassList: ["foobar.com"],
+    },
+  };
+  chrome.proxy.settings.set({ value: config, scope: "regular" }, function () {
+    console.log("setting proxy success");
+  });
+
+  let webRTC_value = "default";
+  if (res.tiny_proxy_block_webrtc) {
+    webRTC_value = "disable_non_proxied_udp";
+  }
+  chrome.privacy.network.webRTCIPHandlingPolicy.set({
+    value: webRTC_value,
+  });
+};
 
 chrome.runtime.onInstalled.addListener(updateFunction);
-function updateFunction() {
-    chrome.storage.local.get(['tiny_proxy_scheme', 'tiny_proxy_host', 'tiny_proxy_port', 'tiny_proxy_desc'], function(res) {
-    let scheme = res.tiny_proxy_scheme;
-    let host = res.tiny_proxy_host;
-    let port = parseInt(res.tiny_proxy_port);
-    console.log('aaaa', scheme, host, port)
-    let proxy_config = {
-      scheme: scheme,
-      host: host,
-      port: port
-    }
-    console.log('bbbb', proxy_config)
-    let config = {
-      mode: "fixed_servers",
-      rules: {
-        proxyForHttp: proxy_config,
-        proxyForHttps: proxy_config,
-        proxyForFtp: proxy_config,
-        bypassList: ["foobar.com"]
-      }
-    };
-    chrome.proxy.settings.set(
-      {value: config, scope: 'regular'},
-      function() {
-        console.log('ccccc')
-      }
-    );
-  });
-}
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-    if(request.greeting === "update_proxy"){
-      updateFunction() 
-    }
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.greeting === "update_proxy") {
+    updateFunction();
+  }
 });
-// var config = {
-//   mode: "fixed_servers",
-//   rules: {
-//     proxyForHttp: {
-//       scheme: "http",
-//       host: "127.0.0.1",
-//       port: 50013
-//     },
-//     proxyForHttps: {
-//       scheme: "http",
-//       host: "127.0.0.1",
-//       port: 50013
-//     },
-//     bypassList: ["foobar.com"]
-//   }
-// };
-// chrome.proxy.settings.set(
-//   {value: config, scope: 'regular'},
-//   function() {}
-// );
